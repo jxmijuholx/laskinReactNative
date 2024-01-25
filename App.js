da868eebc97203f40f43a5a50731b60fec30218f
jxmijuholx <jami.juhola01@gmail.com>
2024-01-25 21:46:32 +0000

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [result, setResult] = useState("");
+
+  const buttonPressed = (operation) => {
+    const num1Value = parseFloat(num1);
+    const num2Value = parseFloat(num2);
+
+    if (!isNaN(num1Value) && !isNaN(num2Value)) {
+      if (operation === "+") {
+        setResult(num1Value + num2Value);
+      } else if (operation === "-") {
+        setResult(num1Value - num2Value);
+      }
+    } else {
+      setResult("Syötä kelvolliset numerot");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result}</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setNum1(text)}
+        placeholder="Numero 1"
+        keyboardType="numeric"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setNum2(text)}
+        placeholder="Numero 2"
+        keyboardType="numeric"
+      />
+
+      <Button onPress={() => buttonPressed("+")} title="+" />
+      <Button onPress={() => buttonPressed("-")} title="-" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +54,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    margin: 10,
+    width: 200,
   },
 });
